c95dfc29ff0e729878870f5acb999982f39e9927
Damian Dematteo <dematteodamian@gmail.com>
2022-06-14 14:00:00 -0400

    added primitive movement functions and object classes

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ import math
 import random
 import pyinputplus as pyip
 import copy
+import keyboard
+from time import sleep
 
 playing = True
 gameMap = []
@@ -11,8 +13,6 @@ mapSizeX = 10
 mapSizeY = 10
 populationChance = 50
 emptyCharacter = " "
-wallCharacter = "#"
-doorCharacter = "{"
 maxTries = int(mapSizeX * mapSizeY)
 minTries = int(maxTries / 2)
 minRooms = 1
@@ -21,7 +21,113 @@ seedCharacters = 9
 currentSeed = None
 inputSeed = None
 
-globalObjectPool = ['$', '$', '$', '%', '%', '%', '^', '^', '*', '*', 'A', 'H', '&']
+initObjectPool = []
+mapObjectPool = []
+
+### CLASSES ###
+
+class Object:
+    '''Basic object class'''
+    character = " "
+
+    def __init__(self, character=' ', isSolidObject=True, startPos = (None, None)):
+        self.character = character
+        self.isSolidObject = isSolidObject
+        self.x, self.y = startPos
+        if not(self.x == None or self.y == None):
+            self.setPos(self.x, self.y)
+
+    def interact():
+        print("You could not comprehend the object.")
+
+    def remove(self):
+        gameMap[self.x][self.y] == emptyCharacter
+        mapObjectPool.remove(self)
+
+    def setPos(self, x, y):
+        try:
+            if gameMap[x][y] == emptyCharacter:
+                if not(self.x == None or self.y == None): #check if the object is on the map
+                    if gameMap[self.x][self.y] == self.character: # set the map to empty if you were at that point before
+                        gameMap[self.x][self.y] = emptyCharacter
+                self.x = x
+                self.y = y# set internal position
+                gameMap[self.x][self.y] = self.character # set map character
+            else:
+                print("Space is already occupied.")
+        except:
+            print("Space is out of bounds")
+
+class Door(Object):
+    '''Openable and closable door object'''
+    character = "{"
+
+    def __init__(self, coords):
+        super().__init__("{", startPos=coords)
+
+class Wall(Object):
+    '''Immovable/unbreakable wall object'''
+    character = "#"
+
+    def __init__(self, coords):
+        super().__init__("#", startPos=coords)
+
+class Consumable(Object):
+    '''Consumable class, can use to give the player stats or health'''
+
+    def __init__(self, character='&', healPoints=0, speedIncrease=0, dmgIncrease=0, healthIncrease=0, xpIncrease=0):
+        super().__init__(character)
+        self.healPoints = healPoints
+        self.speedIncrease = speedIncrease
+        self.dmgIncrease = dmgIncrease
+        self.xpIncrease = xpIncrease
+
+    def consume(player):
+        player.hp += self.healPoints
+        player.speed += self.speedIncrease
+        player.damage += self.dmgIncrease
+        player.xp += self.xpIncrease
+
+class Enemy(Object):
+    '''Enemy class, attacks player'''
+
+    def __init__(self):
+        super().__init__(self)
+
+class Player(Object):
+    '''Player class that controls position, stats, and inventory'''
+    character = "@"
+    maxHP = 100
+
+    def __init__(self, startCoords, hp=maxHP, attack=1, defense=1, speed=1, startingItems = []):
+        super().__init__(self.character, startCoords)
+        self.hp = hp
+        self.xp = 0
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.inventory = startingItems
+        if startCoords:
+            x, y = startCoords
+            self.setPos(x, y)
+
+    def move(self, moveCoords, numMoves):
+        moveX, moveY = moveCoords
+        for x in range(0, numMoves):
+            self.setPos(self.x + moveX, self.y + moveY)
+
+### OBJECT FUNCTIONS ###
+
+def findObjectByCoords(objectPool, coords):
+    cordX, cordY = coords
+    for item in objectPool:
+        if item.x == cordX and item.y == cordY:
+            return item
+
+### PLAYER FUNCTIONS ###
+
+
+### NUMBER GENERATOR FUNCTIONS ###
 
 def GenerateSeed(charAmt=9):
     seed = ""
@@ -55,6 +161,30 @@ def InitGen(seedCharacters):
     random.seed(currentSeed)
     return currentSeed
 
+### MAP FUNCTIONS ###
+
+def PopulateObjectPool(pool, maxConsumables, maxEnemies):
+    for x in range(0, random.randint(maxConsumables / 2, maxConsumables)):
+        for z in range(0, 2):
+            chance = random.randint(0, 3)
+            hp = 0
+            sI = 0
+            dI = 0
+            hI = 0
+            if chance == 0:
+                hp += random.randint(Player.maxHP / 2, Player.maxHP)
+            elif chance == 1:
+                sI += 1
+            elif chance == 2:
+                dI += 1
+            else:
+                hI += 1
+
+        pool.append(Consumable('&', hp, sI, dI, hI))
+    #for y in range(0, random.randint(maxEnemies / 2, maxEnemies)):
+        #add enemy to object pool
+
+
 def InitMap(mapSizeX, mapSizeY, gameMap, currentSeed):
     '''Creates a virtual map with an area of mapSizeX * mapSizeY and initializes the random number generator.'''
     for x in range(mapSizeX):
@@ -65,41 +195,45 @@ def InitMap(mapSizeX, mapSizeY, gameMap, currentSeed):
 def CreateRooms(minRooms, maxRooms):
     '''Creates rooms randomly by creating intersecting walls'''
     for i in range(random.randint(minRooms, maxRooms)):
-        startX = random.randint(1, mapSizeX - 1) # starting point along X axis
-        startY = random.randint(1, mapSizeY - 1) # starting point along Y axis
+        topCorner_l = (random.randint(0, mapSizeX - 5), random.randint(2, mapSizeY - 2))
+        bottomCorner_r = (random.randint(topCorner_l[0] + 2, mapSizeX - 1), random.randint(0, topCorner_l[1] - 2))
+        topCorner_r = (bottomCorner_r[0], topCorner_l[1])
+        bottomCorner_l = (topCorner_l[0], bottomCorner_r[1])
 
-        if ((not gameMap[0][startY] == emptyCharacter) or (not gameMap[startX][0] == emptyCharacter)):
-            continue
+        if gameMap[topCorner_l[0]][topCorner_l[1]] == Wall.character: continue
 
-        tileList = []
+        if gameMap[topCorner_r[0]][topCorner_r[1]] == Wall.character: continue
 
-        wallLocX = 0 # used for creating the wall
-        for x in range(startX):
-            gameMap[wallLocX][startY] = wallCharacter
-            tileList.append((wallLocX, startY))
-            wallLocX += 1
+        if gameMap[bottomCorner_l[0]][bottomCorner_l[1]] == Wall.character: continue
 
-        wallLocY = 0
-        for y in range(startY):
-            gameMap[startX][wallLocY] = wallCharacter
-            tileList.append((startX, wallLocY))
-            wallLocY += 1
+        if gameMap[bottomCorner_r[0]][topCorner_r[1]] == Wall.character: continue
 
-        chosenTileCoords = tileList[random.randint(0, len(tileList) - 1)]
-        gameMap[chosenTileCoords[0]][chosenTileCoords[1]] = doorCharacter # turn a random tile in the wall into a door
+        for x in range(topCorner_l[0], topCorner_r[0]):
+            mapObjectPool.append(Wall((x, topCorner_l[1])))
+
+        for x1 in range(bottomCorner_l[0], bottomCorner_r[0]):
+            mapObjectPool.append(Wall((x1, bottomCorner_l[1])))
+
+        for y in range(bottomCorner_l[1], topCorner_l[1]):
+            mapObjectPool.append(Wall((bottomCorner_l[0], y)))
+
+        for y1 in range(bottomCorner_r[1], topCorner_r[1]):
+            mapObjectPool.append(Wall((bottomCorner_r[0], y1)))
+
+        mapObjectPool.append(Wall(topCorner_r))
 
 def PopulateMap(minTries, maxTries, populationChance, objectPool):
     '''Places objects from the object pool randomly around the map'''
     localObjectPool = copy.deepcopy(objectPool)
 
     curPoint = (random.randint(0, mapSizeX - 1), random.randint(0, mapSizeY - 1))
-    print(gameMap[curPoint[0]][curPoint[1]])
 
     for i in range(random.randint(minTries, maxTries)):
         if ((random.randint(0, 100) > (populationChance - 1) and len(localObjectPool) > 0)):
             if (gameMap[curPoint[0]][curPoint[1]] == emptyCharacter):
                 itemToAdd = localObjectPool[random.randrange(0, len(localObjectPool))]
-                gameMap[curPoint[0]][curPoint[1]] = itemToAdd
+                mapObjectPool.append(itemToAdd)
+                itemToAdd.setPos(curPoint[0], curPoint[1])
                 localObjectPool.remove(itemToAdd)
 
         try:
@@ -127,33 +261,42 @@ def DrawMap():
     for i in reversed(range(0, mapSizeY)):
         rowToPrint = ""
         for z in range(0, len(columns)):
-             rowToPrint += f" {gameMap[z][i]} "
+            rowToPrint += f" {gameMap[z][i]} "
 
         print(rowToPrint + '\n')
 
-def Main(): # Unused function
-    return
-
 #Initialize
-currentSeed = InitGen(seedCharacters)
+#currentSeed = InitGen(seedCharacters)
 InitMap(mapSizeX, mapSizeY, gameMap, currentSeed) #put clear map function before if used again in the program.
+PopulateObjectPool(initObjectPool, 4, 10)
+currentSeed = ChangeSeed(GenerateSeed())
+ClearMap()
+PlayerObj = Player((0, 0))
+mapObjectPool.append(PlayerObj)
+CreateRooms(minRooms, maxRooms)
+PopulateMap(minTries, maxTries, populationChance, initObjectPool)
+DrawMap()
 
 while (playing):
+    nextTurn = False
+    while (not nextTurn):
+        usrInput = keyboard.read_key()
+        if (usrInput == "w"):
+            nextTurn = True
+            move = (0, 1)
+        if (usrInput == "s"):
+            nextTurn = True
+            move = (0, -1)
+        if (usrInput == "a"):
+            nextTurn = True
+            move = (-1, 0)
+        if (usrInput == "d"):
+            nextTurn = True
+            move = (1, 0)
+
+        PlayerObj.move(move, 1)
+
+    DrawMap()
     print('Current seed:' + str(currentSeed))
-    playerInput = pyip.inputMenu(['Generate New Map', 'Generate New Seed', 'Input Seed', 'Save Current Seed', 'View Saved Seeds', 'Exit'], numbered=True)
-    if (playerInput == 'Generate New Map'):
-        ClearMap()
-        CreateRooms(minRooms, maxRooms)
-        PopulateMap(minTries, maxTries, populationChance, globalObjectPool)
-        DrawMap()
-    elif (playerInput == 'Generate New Seed'):
-        currentSeed = InitGen(seedCharacters)
-    elif (playerInput == 'Input Seed'):
-        seedInput = pyip.inputInt("\n")
-        currentSeed = ChangeSeed(seedInput)
-    elif (playerInput == 'Save Current Seed'):
-        SaveSeed(currentSeed)
-    elif (playerInput == 'View Saved Seeds'):
-        ViewSeeds()
-    else:
-        playing = False
+    sleep(0.1)
+
